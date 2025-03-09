f8b41a3167e6eacc86d9952cd74c4382ac8ecc61
Pedro Reis <itspdro@outlook.com>
2025-03-09 18:07:00 -0300

    fix workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { dev } from "$app/environment";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
@@ -12,6 +13,9 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+      base: "",
+    },
   },
 };
 
